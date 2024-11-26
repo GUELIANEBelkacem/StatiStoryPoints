54de974fae7cb8c44ea9ec0bd3af1a334e2e88c8
GUELIANEBelkacem <mohamed.gueliane@gmail.com>
2024-11-26 02:48:17 +0100

    test

--- js/client.js
+++ js/client.js
@@ -79,60 +79,67 @@ t.getAll();
 var ICON = './images/ic_story_point.png';
 
 
-var randomBadgeColor = function () {
-  return ['green', 'yellow', 'red', 'none'][Math.floor(Math.random() * 4)];
-};
+// var randomBadgeColor = function () {
+//   return ['green', 'yellow', 'red', 'none'][Math.floor(Math.random() * 4)];
+// };
 
 var getBadges = function (t) {
-  return t.card('name')
-    .get('name')
-    .then(function (cardName) {
-      console.log('We just loaded the card name for fun: ' + cardName);
+  return t.card('id')
+    .get('id')
+    .then(function (id) {
+      return t.get('card', 'shared', `stati_story_point_value_${id}`)
+    })
+    .then(function (val) {
+      console.log('loading detailed badges');
 
-      return [{
-        // dynamic badges can have their function rerun after a set number
-        // of seconds defined by refresh. Minimum of 10 seconds.
-        dynamic: function () {
-          // we could also return a Promise that resolves to this as well if we needed to do something async first
-          return {
-            title: 'Detail Badge', // for detail badges only
-            text: 'Dynamic ' + (Math.random() * 100).toFixed(0).toString(),
-            icon: ICON, // for card front badges only
-            color: randomBadgeColor(),
-            refresh: 10 // in seconds
-          };
+      return [
+        //   {
+        //   // dynamic badges can have their function rerun after a set number
+        //   // of seconds defined by refresh. Minimum of 10 seconds.
+        //   dynamic: function () {
+        //     // we could also return a Promise that resolves to this as well if we needed to do something async first
+        //     return {
+        //       title: 'Detail Badge', // for detail badges only
+        //       text: 'Dynamic ' + (Math.random() * 100).toFixed(0).toString(),
+        //       icon: ICON, // for card front badges only
+        //       color: randomBadgeColor(),
+        //       refresh: 10 // in seconds
+        //     };
+        //   }
+        // }, 
+        {
+          // its best to use static badges unless you need your badges to refresh
+          // you can mix and match between static and dynamic
+          title: 'StatiStoryPoints', // for detail badges only
+          text: val,
+          icon: ICON, // for card front badges only
+          color: 'green'
         }
-      }, {
-        // its best to use static badges unless you need your badges to refresh
-        // you can mix and match between static and dynamic
-        title: 'Detail Badge', // for detail badges only
-        text: 'Static',
-        icon: ICON, // for card front badges only
-        color: null
-      }, {
-        // card detail badges (those that appear on the back of cards)
-        // also support callback functions so that you can open for example
-        // open a popup on click
-        title: 'Popup Detail Badge', // for detail badges only
-        text: 'Popup',
-        icon: ICON, // for card front badges only
-        callback: function (context) { // function to run on click
-          return context.popup({
-            title: 'Card Detail Badge Popup',
-            url: './settings.html',
-            height: 184 // we can always resize later, but if we know the size in advance, its good to tell Trello
-          });
-        }
-      }, {
-        // or for simpler use cases you can also provide a url
-        // when the user clicks on the card detail badge they will
-        // go to a new tab at that url
-        title: 'URL Detail Badge', // for detail badges only
-        text: 'URL',
-        icon: ICON, // for card front badges only
-        url: 'https://trello.com/home',
-        target: 'Trello Landing Page' // optional target for above url
-      }];
+        // , {
+        //   // card detail badges (those that appear on the back of cards)
+        //   // also support callback functions so that you can open for example
+        //   // open a popup on click
+        //   title: 'Popup Detail Badge', // for detail badges only
+        //   text: 'Popup',
+        //   icon: ICON, // for card front badges only
+        //   callback: function (context) { // function to run on click
+        //     return context.popup({
+        //       title: 'Card Detail Badge Popup',
+        //       url: './settings.html',
+        //       height: 184 // we can always resize later, but if we know the size in advance, its good to tell Trello
+        //     });
+        //   }
+        // }, {
+        //   // or for simpler use cases you can also provide a url
+        //   // when the user clicks on the card detail badge they will
+        //   // go to a new tab at that url
+        //   title: 'URL Detail Badge', // for detail badges only
+        //   text: 'URL',
+        //   icon: ICON, // for card front badges only
+        //   url: 'https://trello.com/home',
+        //   target: 'Trello Landing Page' // optional target for above url
+        // }
+      ];
     });
 };
 
@@ -209,30 +216,28 @@ var cardButtonCallback = function (t, opts) {
       text: fibItem,
       callback: function (t) {
 
-        console.log('object t');
-        console.log(t);
-        console.log('object opts');
-        console.log(opts);
+        // console.log('object t');
+        // console.log(t);
+        // console.log('object opts');
+        // console.log(opts);
 
-        console.log('t.card()');
-        console.log(t.card());
-        console.log('t.card(name)');
-        console.log(t.card('name'))
-        console.log('t.card(id, name)');
-        console.log(t.card('id', 'name'))
-        console.log('t.card(all)');
-        console.log(t.card('all'));
+        // console.log('t.card()');
+        // console.log(t.card());
+        // console.log('t.card(name)');
+        // console.log(t.card('name'))
+        // console.log('t.card(id, name)');
+        // console.log(t.card('id', 'name'))
+        // console.log('t.card(all)');
+        // console.log(t.card('all'));
 
-        t.card('name')
-          .get('name')
-          .then(function (cardName) {
-            console.log('Heres the card name: ' + cardName);
-          });
-
-        return t.set('card', 'private', 'storyPoints', fibItem)
+        t.card('id')
+          .get('id')
+          .then(function (id) { set('card', 'shared', `stati_story_point_value_${id}`, fibItem) })
           .then(function () {
+            console.log('value set');
             return t.closePopup();
           });
+
       }
     };
   });
@@ -324,9 +329,9 @@ TrelloPowerUp.initialize({
       target: 'Inspiring Boards' // optional target for above url
     }];
   },
-  'card-badges': function (t, options) {
-    return getBadges(t);
-  },
+  // 'card-badges': function (t, options) {
+  //   return getBadges(t);
+  // },
   'card-buttons': function (t, options) {
     return [{
       // usually you will provide a callback function to be run on button click
