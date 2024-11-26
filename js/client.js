fb44da2443965672e6afe88c8a67b7aca5dbc446
GUELIANEBelkacem <mohamed.gueliane@gmail.com>
2024-11-26 05:25:07 +0100

    test

--- js/client.js
+++ js/client.js
@@ -119,11 +119,11 @@ var updateLists = function (t) {
           var cardID = card.id
           console.log("id")
           console.log(cardID)
-          var x = 0//t.get(cardID, 'shared', `stati_story_point_value_${cardID}`)
+          var x = t.get(cardID, 'shared', `stati_story_point_value_${cardID}`)
           console.log("x")
           console.log(x)
           xx = parseInt(x)
-          if (xx) spcount += 
+          if (xx) spcount += xx
 
         });
 
