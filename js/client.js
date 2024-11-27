19a0f9fcfe16da9e45514d8f9dbbb076dcd46768
GUELIANEBelkacem <mohamed.gueliane@gmail.com>
2024-11-27 01:03:29 +0100

    test

--- js/client.js
+++ js/client.js
@@ -127,21 +127,17 @@ function setColumnName(t, id, name) {
 
 var getTotalListSPCountBadge = async function (t) {
   var list = await t.list('all');
+  const card = await t.card('name')
   var spcount = await getTotalListSPCount(t, list)
 
   const listName = list.name
 
-  // $("#board .list").each(function (_, l) {
-  //   $(".list-header-name", l).append(" (" + spcount + ")");
-  // });
-  var xxx = document.querySelectorAll('*');
-
-  console.log(xxx)
-
-  console.log('listName')
-  console.log(listName)
-  console.log('list id')
-  console.log(list.id)
+  if (card.name.indexOf('|Recap') === 0) {
+    await t.attach({
+      name: 'Recap image', // optional
+      url: 'https://fakeimg.pl/600x400/a65858/543939?text=Hello' // required
+    });
+  }
 
   return {
     title: 'Totale Story Points',
@@ -173,6 +169,7 @@ var getBadges = async function (t) {
   const id = card.id
 
   const sp = await t.get('board', 'shared', `stati_story_point_value_${id}`);
+
 
   return [
     {
@@ -362,54 +359,54 @@ TrelloPowerUp.initialize({
   // If you need to make an asynchronous request or action before you can reply to Trello
   // you can return a Promise (bluebird promises are included at TrelloPowerUp.Promise)
   // The Promise should resolve to the object type that is expected to be returned
-  'attachment-sections': function (t, options) {
-    // options.entries is a list of the attachments for this card
-    // you can look through them and 'claim' any that you want to
-    // include in your section.
+  // 'attachment-sections': function (t, options) {
+  //   // options.entries is a list of the attachments for this card
+  //   // you can look through them and 'claim' any that you want to
+  //   // include in your section.
 
-    // console.log('attachment-sections');
-    // console.log(options);
-    // // we will just claim urls for Yellowstone
-    var claimed = options.entries.filter(function (attachment) {
-      return attachment.url.indexOf('https://fakeimg.pl/') === 0;
-    });
+  //   // console.log('attachment-sections');
+  //   // console.log(options);
+  //   // // we will just claim urls for Yellowstone
+  //   var claimed = options.entries.filter(function (attachment) {
+  //     return attachment.url.indexOf('https://fakeimg.pl/') === 0;
+  //   });
 
-    // // you can have more than one attachment section on a card
-    // // you can group items together into one section, have a section
-    // // per attachment, or anything in between.
-    // if (claimed && claimed.length > 0) {
-    //   // if the title for your section requires a network call or other
-    //   // potentially length operation you can provide a function for the title
-    //   // that returns the section title. If you do so, provide a unique id for
-    //   // your section
-    //   return [{
-    //     id: 'testtest', // optional if you aren't using a function for the title
-    //     claimed: claimed,
-    //     icon: ICON,
-    //     title: 'StatiStoryPoints Section',
-    //     content: {
-    //       type: 'iframe',
-    //       url: t.signUrl('./section.html', { arg: 'you can pass your section args here' }),
-    //       height: 230
-    //     }
-    //   }];
-    // } else {
-    //   return [];
-    // }
-    return [
-      {
-        id: 'testtest', // optional if you aren't using a function for the title
-        claimed: options.entries,
-        icon: ICON,
-        title: 'StatiStoryPoints Section',
-        content: {
-          type: 'image',
-          url: 'https://fakeimg.pl/600x400/a65858/543939?text=Hello',
-          height: 230
-        }
-      }
-    ];
-  },
+  //   // // you can have more than one attachment section on a card
+  //   // // you can group items together into one section, have a section
+  //   // // per attachment, or anything in between.
+  //   // if (claimed && claimed.length > 0) {
+  //   //   // if the title for your section requires a network call or other
+  //   //   // potentially length operation you can provide a function for the title
+  //   //   // that returns the section title. If you do so, provide a unique id for
+  //   //   // your section
+  //   //   return [{
+  //   //     id: 'testtest', // optional if you aren't using a function for the title
+  //   //     claimed: claimed,
+  //   //     icon: ICON,
+  //   //     title: 'StatiStoryPoints Section',
+  //   //     content: {
+  //   //       type: 'iframe',
+  //   //       url: t.signUrl('./section.html', { arg: 'you can pass your section args here' }),
+  //   //       height: 230
+  //   //     }
+  //   //   }];
+  //   // } else {
+  //   //   return [];
+  //   // }
+  //   return [
+  //     {
+  //       id: 'testtest', // optional if you aren't using a function for the title
+  //       claimed: options.entries,
+  //       icon: ICON,
+  //       title: 'StatiStoryPoints Section',
+  //       content: {
+  //         type: 'image',
+  //         url: 'https://fakeimg.pl/600x400/a65858/543939?text=Hello',
+  //         height: 230
+  //       }
+  //     }
+  //   ];
+  // },
   // 'attachment-thumbnail': function (t, options) {
   //   // options.url has the url of the attachment for us
   //   // return an object (or a Promise that resolves to it) with some or all of these properties:
